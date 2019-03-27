2c59070a9b33d86845a9cd07072f6e968a60d8c6
BMarcelus <bmdizon@uci.edu>
2019-03-26 18:14:34 -0700

    bullet effects

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+  public GameObject bulletEffect;
 	void OnCollisionEnter2D(Collision2D collision)
 	{
 		if(DEBUGFLAGS.COLLISIONS) Debug.Log("collision");
 		Lib.FindInHierarchy<IDamagable>(collision.otherCollider.gameObject)?.Damage(1, gameObject);
+    BulletEffect();
 		Destroy(gameObject);
 	}
 	void OnTriggerEnter2D(Collider2D col)
 	{
 		if(DEBUGFLAGS.COLLISIONS) Debug.Log("trigger");
 		Lib.FindInHierarchy<IDamagable>(col.gameObject)?.Damage(1, gameObject);
-		Destroy(gameObject);
+    BulletEffect();
+    Destroy(gameObject);
 	}
+
+  void BulletEffect() {
+    Quaternion rot = Quaternion.LookRotation(-GetComponent<Rigidbody2D>().velocity);
+    GameObject effect = Instantiate(bulletEffect, transform.position, rot);
+		Destroy(effect, 1f);
+  }
 }
