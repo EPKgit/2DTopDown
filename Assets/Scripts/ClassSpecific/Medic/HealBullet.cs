c0012e5ebc53f88ee46d1d55ee3ae99c88914faa
BMarcelus <bmdizon@uci.edu>
2019-05-01 13:16:05 -0700

    make heal bullet get trail renderer in children

--- Assets/Scripts/ClassSpecific/Medic/HealBullet.cs
+++ Assets/Scripts/ClassSpecific/Medic/HealBullet.cs
@@ -11,7 +11,7 @@ public class HealBullet : BaseProjectile
 	public override void Reset()
 	{
 		base.Reset();
-		GetComponent<TrailRenderer>().Clear();
+		GetComponentInChildren<TrailRenderer>().Clear();
 	}
 
 	public void Setup(Vector3 pos, Vector3 direction, GameObject p, float d)
