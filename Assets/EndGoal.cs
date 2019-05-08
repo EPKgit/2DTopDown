64857297b0df60102dcbbd47e5d850a7139981f0
BMarcelus <bmdizon@uci.edu>
2019-05-08 13:18:19 -0700

    end goal testing

---
+++ Assets/EndGoal.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGoal : MonoBehaviour
+{
+  public int count;
+  private bool debug = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if(count == PlayerInput.all.Count) {
+        if(debug)Debug.Log("Win");
+      }
+    }
+
+    void OnTriggerEnter2D(Collider2D col) {
+      if(debug)Debug.Log("Trigger");
+      count += 1;
+    }
+
+    void OnTriggerExit2D(Collider2D col) {
+      count -= 1;
+    }
+}
