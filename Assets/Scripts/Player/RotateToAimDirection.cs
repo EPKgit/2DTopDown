250483bb804034906593e5caa0ed717201b394aa
BMarcelus <bmdizon@uci.edu>
2019-05-08 14:51:22 -0700

    test bois

--- Assets/Scripts/Player/RotateToAimDirection.cs
+++ Assets/Scripts/Player/RotateToAimDirection.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class RotateToAimDirection : MonoBehaviour
 {
+    public bool useYinstead = false;
+    public float offset = 0;
+    public bool flip = false;
     private PlayerInput playerInput;
     // Start is called before the first frame update
     void Start()
@@ -14,11 +17,15 @@ public class RotateToAimDirection : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 angles = transform.rotation.eulerAngles;
+        Vector3 angles = transform.localEulerAngles;
         Vector3 aimDirection = -playerInput.GetAimDirection();
-        angles.z = Mathf.Atan2(aimDirection.y, aimDirection.x)*Mathf.Rad2Deg;
+        float sign = 1;
+        if(flip)sign=-1;
+        float angle = sign*Mathf.Atan2(aimDirection.y, aimDirection.x)*Mathf.Rad2Deg+180+offset;
+        if(useYinstead) angles.y = angle;
+        else angles.z = angle;
         Quaternion target = Quaternion.Euler(angles);
         // Quaternion target = Quaternion.LookRotation(aimDirection, Vector3.up);
-        transform.rotation = Quaternion.Lerp(transform.rotation, target, 0.5f);
+        transform.localRotation = Quaternion.Lerp(transform.localRotation, target, 0.5f);
     }
 }
